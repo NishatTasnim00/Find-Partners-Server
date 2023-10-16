054a06bb0232688e94d7db07b03465a84b5e079a
Nishat <tasnim.nt00@gmail.com>
2023-10-16 20:02:33 +0600

    data setup

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 3000;
+
+//middle wire
+app.use(cors());
+app.use(express.json());
+
+const charteredAccountants = require('./db.json');
+
+
+
+
+
+
+
+
+
+
+
+app.get('/', (req, res) => {
+	res.send(
+        // 'find partners is running...'
+        charteredAccountants[1]
+    );
+});
+app.listen(port, () => {
+	console.log('find partners is running...');
+});
